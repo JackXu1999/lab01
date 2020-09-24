bfb183a7c85807de677b5a454d2d1e2d55fc7c31
Jack Xu <jackxu1999@hotmail.com>
2020-09-25 01:14:42 +0800

    fixed pointer bugs after talking with the prof

--- my_malloc.c
+++ my_malloc.c
@@ -7,7 +7,14 @@ struct freelistnode *my_list;
 //my_list->size = 0;
 //my_list->flink = NULL;
 
-int calSize(size_t size);
+struct returnChunk {
+    int size; //size of current chunk
+    int check;
+};
+
+struct returnChunk *chunk;
+
+int calcSize(size_t size);
 
 int calcSize(size_t size) {
     int remainder = (size + 8) % 8;
@@ -19,24 +26,28 @@ int calcSize(size_t size) {
 void *my_malloc(size_t size) {
     if (my_list == NULL) {
         // when it does not exist, add the first node to the linked list
-        if (size + 8 <= 8192) {
+        if (calcSize(size) <= 8192) {
             my_list = (FreeListNode)sbrk(8192);
             // the minimum size is 8192
 
             my_list->flink = NULL;
 
             // need to split the free space
-            if (8912 - size > 16) {
+            if (8912 - calcSize(size) > 16) {
                 my_list->size = 8192 - calcSize(size);
             } else {
+                // TODO Don't put anything on the freelist
                 my_list->size = 0;
             }
 
             // put size and checksum before the return address
-            int *return_size = &my_list + my_list->size;
-            return_size = size;
-            int *check = &my_list + my_list->size + 4;
-            check = 100;
+            int *return_size;
+            return_size = (int*)((void*)my_list + my_list->size);
+            *return_size = size; // size to go in to the address
+
+            int *check;
+            check = (int*)((void*)my_list + my_list->size + 4);
+            *check = 100;
 
             return &my_list + my_list->size + 8;
         } else {
@@ -45,29 +56,46 @@ void *my_malloc(size_t size) {
             my_list->size = 0;
             my_list->flink = NULL;
 
-            int *return_size = &my_list + my_list->size;
-            return_size = size;
-            int *check = &my_list + my_list->size + 4;
-            check = 100;
+            // put size and checksum before the return address
+            int *return_size;
+            return_size = (int*)((void*)my_list + my_list->size);
+            *return_size = size; // size to go in to the address
+
+            int *check;
+            check = (int*)((void*)my_list + my_list->size + 4);
+            *check = 100;
+
 
             return &my_list + my_list->size + 8;
         }
     } else {
-        // when it already exists, traverse the linked list
-        while (my_list->flink != NULL) {
+        // when it already exists, traverse the linked list, my_list != NULL
+        while (my_list != NULL) {
             int size_of_node = my_list->size;
             int real_size = calcSize(size);
 
             if (size_of_node >= real_size) {
                 // case 1: when we can find the node for memory, but we need to split
                 // case 2: when we can find the node for memory, we do not need to split
-                // int next = size_of_node - real_size;
+
                 my_list->size = size_of_node - real_size;
 
-                int *return_size = &my_list + my_list->size;
-                return_size = size;
-                int *check = &my_list + my_list->size + 4;
-                check = 100;
+                // put size and checksum before the return address
+                int *return_size;
+                return_size = (int*)((void*)my_list + my_list->size);
+                *return_size = size; // size to go in to the address
+
+                int *check;
+                check = (int*)((void*)my_list + my_list->size + 4);
+                *check = 100;
+
+
+                //TODO Logic isn't right somewhere
+
+                if (my_list->size < 16) {
+                    // not enough space for the node to stay on the freelist
+                    my_list->flink = my_list->flink->flink;
+                }
 
                 return &my_list + my_list->size + 8;
 
@@ -95,10 +123,13 @@ void *my_malloc(size_t size) {
             }
 
             // put size and checksum before the return address
-            int *return_size = &my_list + my_list->size;
-            return_size = size;
-            int *check = &my_list + my_list->size + 4;
-            check = 100;
+            int *return_size;
+            return_size = (int*)((void*)my_list + my_list->size);
+            *return_size = size; // size to go in to the address
+
+            int *check;
+            check = (int*)((void*)my_list + my_list->size + 4);
+            *check = 100;
 
             return &my_list + my_list->size + 8;
         } else {
@@ -107,10 +138,15 @@ void *my_malloc(size_t size) {
             my_list->size = 0;
             my_list->flink = NULL;
 
-            int *return_size = &my_list + my_list->size;
-            return_size = size;
-            int *check = &my_list + my_list->size + 4;
-            check = 100;
+            // put size and checksum before the return address
+            int *return_size;
+            return_size = (int*)((void*)my_list + my_list->size);
+            *return_size = size; // size to go in to the address
+
+            int *check;
+            check = (int*)((void*)my_list + my_list->size + 4);
+            *check = 100;
+
 
             return &my_list + my_list->size + 8;
         }
